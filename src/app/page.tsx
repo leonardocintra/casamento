9fd1e8e7f619234cacf6b4efdfa438deed929044
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-09-07 12:36:58 -0300

    🎉 init(font[): vamo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,12 @@ export default function Home() {
           style={{ backgroundImage: `url(${urlImagem})` }}
         >
           <div className="text-white lg:text-blue-950 text-6xl text-end">
-            <h2 className="mt-12 bg-slate-400 bg-opacity-30 p-3 rounded-md mx-5">Marília & Lucas</h2>
-            <h3 className="mt-10 font-extralight text-3xl md:text-5xl">Data casamento: 01/10/2023</h3>
+            <h2 className="mt-12 bg-slate-400 bg-opacity-30 rounded-md mx-5 font-greatVibes text-7xl text-center sm:text-end">
+              Marília & Lucas
+            </h2>
+            <h3 className="mt-10 font-extralight text-3xl md:text-5xl m-4">
+              Data casamento: 01/10/2023
+            </h3>
           </div>
         </div>
       </div>
